83696e9ec95e66ac5a461478479c9d6ef1f96b4d
omartela <oskari@gmail.com>
2024-08-08 13:41:30 +0300

    Adds philo.h header file

---
+++ includes/philo.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/08 13:29:12 by omartela          #+#    #+#             */
+/*   Updated: 2024/08/08 13:31:40 by omartela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+#endif
