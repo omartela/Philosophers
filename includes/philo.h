b98c338dd0c37476b91bca1a4777c6ec55d6d4f1
omartela <oskari.martela@gmail.com>
2024-08-09 12:18:52 +0300

    Progressing... starting to create validate user input

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 13:29:12 by omartela          #+#    #+#             */
-/*   Updated: 2024/08/08 13:31:40 by omartela         ###   ########.fr       */
+/*   Updated: 2024/08/09 12:17:33 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,30 @@
 # include <sys/time.h>
 # include <unistd.h>
 
+typedef struct s_philo
+{
+	t_program		*program;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*r_fork;
+	size_t			last_eat;
+	int				no_meals;
+	int				id;
+	pthread_t		t;	
+}	t_philo;
+
+typedef struct s_program
+{
+	int				no_philos;
+	t_philo			*philos;
+	pthread_mutex_t	lock;
+	pthread_mutex_t	*forks;
+	size_t			eat_time;
+	size_t			die_time;
+	size_t			sleep_time;
+	size_t			start_time;
+	int				no_full;
+	size_t			philos_full;
+	size_t			philo_dead;
+	int				no_meals;
+}	t_program;
 #endif
---
+++ src/parseinput.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parseinput.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 10:55:06 by omartela          #+#    #+#             */
+/*   Updated: 2024/08/09 12:16:34 by omartela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "philo.h"
+
+void	ft_message(void)
+{
+	printf("Program should take the following arguments: \n");
+	printf("Mandatory: Number of philosophers > 0 \n");
+	printf("Mandatory: The time when philosopher dies \
+		if he does not eat > 0 \n");
+	printf("Mandatory: The time it takes to philosopher eat > 0 \n");
+	printf("Mandatory: Time to sleep > 0 \n");
+	printf("Optional: Number of times each philosopher \
+			must eat to become full >= 0 \n");
+	exit(1);
+}
+
+int	ft_str_to_int(char *str, int no_zero)
+{
+	long int	res;
+
+	res = 0;
+
+	while (*str == ' ' || *str == '\t' || *str == '\v'
+			|| *str == '\r' || *str == '\f' || *str == '\n')
+		++str;
+	if (*str == '-')
+		ft_message();
+	if (*str == '+')
+		++str;
+	while (*str >= '0' && *str <= '9')
+	{
+		res = res * 10 + *str++ - '0';
+		if (res > 2147483647)
+			ft_message();
+	}
+	if (*str || (no_zero && res == 0))
+		ft_message();
+	return ((int)res);
+}
+
+void	ft_parseinput(t_program *program, char **str, int ac)
+{
+	if (ac < 5 || 6 > ac)
+		ft_message();
+	*program->no_philos = ft_str_to_int(str[1], 1);
+	if (ac == 6)
+		program->no_meals = ft_str_to_int(str[5], 0);
+	else
+		program->no_meals = -1;
+}
