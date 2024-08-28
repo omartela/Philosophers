a5090d54c6bdbdd72601d6fad485b7a961e7b825
omartela <oskari@gmail.com>
2024-08-28 12:53:48 +0300

    Removes unnecessary comme from print with one philo

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: omartela <omartela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 14:28:08 by omartela          #+#    #+#             */
-/*   Updated: 2024/08/26 17:29:47 by omartela         ###   ########.fr       */
+/*   Updated: 2024/08/28 12:52:50 by omartela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/philo.h"
@@ -25,7 +25,7 @@ static void	ft_one_philo(t_philo *philo)
 
 	pthread_mutex_lock(philo->l_fork);
 	start_time = philo->program->start_time;
-	printf("%zu, %d has taken a fork\n", get_current_time() - start_time, 1);
+	printf("%zu %d has taken a fork\n", get_current_time() - start_time, 1);
 	ft_wait(philo->program->die_time, &philo->program->philos[0]);
 	pthread_mutex_unlock(philo->l_fork);
 }
